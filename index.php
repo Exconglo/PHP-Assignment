dbdb364d01e2355302fba256aa650d303fa3af13
Excellence Chukwunaedu <99671287+Exconglo@users.noreply.github.com>
2022-03-07 16:23:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+//This is an example of a single line comment
+
+/*
+This is an example of a multi-line comment
+*/
+
+define('HI', 'Greeting');
+echo HI;
+echo '<br/>';
+echo '<br/>';
+//This is an example of a constant
+
+$a = 'come';
+$b = 'go';
+echo $a;
+echo '<br/>';
+echo $b;
+echo '<br/>';
+echo '<br/>';
+
+//This is an Integer
+$new = 10;
+//This is a float
+$old = 15.235;
+echo $new;
+echo '<br/>';
+echo $old;
+
+//Concatenation (Dot)
+echo '<h1>'.$a.' '.$b.'! </h1>';
+echo '<br/>';
+
+?>
